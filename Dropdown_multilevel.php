7ed7c5841662a01a8839b3890fd36ab9d9b4a3bf
Kailash Meghwar <premanikailash97@gmail.com>
2022-03-06 19:46:18 +0500

    My Git Multilevel Dropdown

---
+++ Dropdown_multilevel.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Multi-Level Drop Down Menu with Pure CSS</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body bgcolor="skyblue">
+ 
+  <h1 align="center" style="color: red;">Multilevel Dropdown</h1>
+  
+  <ul class="main-navigation">
+  <li><a href="#">Home</a></li>
+
+  <li><a href="#">Corses</a>
+      <ul>
+      <li><a href="#">HTML</a>
+       <ul>
+          <li><a href="#">Element</a></li>
+          <li><a href="#">Tags</a></li>
+          <li><a href="#">Scope Veriables</a></li>
+         </ul>
+      </li>
+      <li><a href="#">CSS</a>
+          <ul>
+          <li><a href="#">Girds</a></li>
+          <li><a href="#">Properties</a></li>
+          <li><a href="#">Selectors</a></li>
+         </ul>
+        </li>
+
+      <li><a href="#">JavaScript</a>
+      <ul>
+          <li><a href="#">Loops</a></li>
+          <li><a href="#">Events</a></li>
+      </ul>
+      </li>
+      </ul>
+      </li>
+
+
+  <li><a href="#">WordPress Development</a>
+    <ul>
+      <li><a href="#">Themes</a></li>
+      <li><a href="#">Plugins</a></li>
+      <li><a href="#">Custom Post Types</a>
+      <ul>
+          <li><a href="#">CMS</a></li>
+          <li><a href="#">Website Create</a></li>
+      </ul>
+  </li>
+  </ul>
+  </li>
+  </ul>
+
+</body>
+</html>
